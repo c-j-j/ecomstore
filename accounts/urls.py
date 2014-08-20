395fe6abf8dfde0b5cdc616fb02056c0f8e77dfa
chrisjordan87 <chrisjordan1987@gmail.com>
2014-08-20 16:47:09 +0100

    Several issue-ettes fixed.. But Django Login not redirecting user to the correct page

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,14 +2,13 @@ from django.conf.urls import patterns, url
 from ecomstore import settings
 
 urlpatterns = patterns('accounts.views',
-    (r'^register/$', 'register', {'SSL': settings.ENABLE_SSL,}, 'register'),
-    url(r'^my_account/$', 'my_account', name='my_account'),
+    url(r'^register/$', 'register', name='register'),
+    url(r'^my_account/$', 'my_account', {'template_name': 'registration/my_account.html'},name='my_account'),
     url(r'^order_details/(?P<order_id>[-\w]+)/$', 'order_details', name='order_details'),
     url(r'^order_info/$', 'order_info', name='order_info'),
 )
 
 urlpatterns += patterns('django.contrib.auth.views',
-    (r'^login/$', 'login', {
-        'template_name': 'registration/login.html',
-        'SSL': settings.ENABLE_SSL
-    },'login'),)
+                        url(r'^login/$', 'login', {
+                            'template_name': 'registration/login.html',
+                        }, name='login'))
